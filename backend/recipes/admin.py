de7cb7171b14a32b697df4e57ba0ce020dc62ed2
Nikita Perkin <perkin1999@gmail.com>
2026-04-03 23:57:37 +0300

    Update Foodgram

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import (
-    Recipe, Tag, Ingredient, RecipeIngredient, Favorite, ShoppingCart)
+from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingCart, Tag)
 
 
 @admin.register(Recipe)
