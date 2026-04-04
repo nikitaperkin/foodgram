a79204b41f013fbd4779ddfbd321eb9eee91df0e
Nikita Perkin <perkin1999@gmail.com>
2026-04-04 23:20:28 +0300

    Update Foodgram

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         data_path = os.path.join(
-            settings.BASE_DIR.parent, 'data', 'ingredients.json'
+            settings.BASE_DIR, 'data', 'ingredients.json'
         )
 
         if not os.path.exists(data_path):
