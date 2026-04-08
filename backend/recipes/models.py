3f2b583ae13798bd4890b453677a77189fc1235e
Nikita Perkin <perkin1999@gmail.com>
2026-04-09 01:01:00 +0300

    Changes after review #1

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,13 +1,10 @@
 from django.core.validators import MinValueValidator, RegexValidator
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-# from django.contrib.auth import get_user_model
 
 
 MIN_COOKING_TIME = 1
 MIN_AMOUNT = 1
-
-# User = get_user_model()
 
 
 class User(AbstractUser):
